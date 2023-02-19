b23b741690d44c15de6d29c8b1224a9f9061308f
Mara Green <maragreen42@gmail.com>
2023-02-19 09:44:07 -0800

    registered Pattern and Photos in urls.py + added test data too database

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
+    # path('api/Patterns/', include('Patterns.urls')),
+    # path('api/Photos/', include('Photos.urls'))
 ]
