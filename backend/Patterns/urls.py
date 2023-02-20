1fbaf4802043fb1cbd586e792567e6f518ec6b14
Mara Green <maragreen42@gmail.com>
2023-02-19 18:39:20 -0800

    added PUT, DELETE, and GET by user funcitons

--- backend/Patterns/urls.py
+++ backend/Patterns/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.patterns),
-    path('<int:pk>/', views.get_patterns_by_user),
+    path('', views.patterns_list),
+    # path('', views.post_new_pattern),
+    path('', views.get_patterns_by_user),
 
 ]
--- backend/Patterns/views.py
+++ backend/Patterns/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 
 
 from .models import Pattern
@@ -14,18 +14,36 @@ from .serlializers import PatternSerializer
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def patterns(response):
-    patterns = Pattern.objects.all()
-    serializer = PatternSerializer(patterns, many=True)
-    return Response(serializer.data)
+def patterns_list(request):
+    if request.method == 'GET':
+        pattern_name = request.query_params.get('pattern_id')
+        patterns = Pattern.objects.all()
+        if pattern_name:
+            patterns = patterns.filter(pattern_id=pattern_name)
+        serializer = PatternSerializer(patterns, many=True)
+        return Response(serializer.data)
+    elif request.mathod == 'POST':
+        serializer = PatternSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+
+@api_view(['GET', 'PUT'])
 @permission_classes([IsAuthenticated])
-def get_patterns_by_user(request):
+def get_patterns_by_user(request, pk):
+    patterns = get_object_or_404(Pattern, pk=pk)
     print(
-        'user:', f"{request.user.username}"
-    )
+        'user:', f"{request.user.username}")
     if request.method == 'GET':
         patterns = Pattern.objects.filter(user_id = request.user.id)
         serializer = PatternSerializer(patterns, many=True)
         return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = PatternSerializer(patterns, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        patterns.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
