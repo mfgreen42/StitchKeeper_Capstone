53cb4fdcae47aa72dc3eeb69f942789af52bd487
Mara Green <maragreen42@gmail.com>
2023-02-25 13:51:32 -0800

    AddPatternForm is working correctly

--- frontend/src/components/AddPatternForm.jsx
+++ frontend/src/components/AddPatternForm.jsx
@@ -2,60 +2,67 @@ import useAuth  from "../hooks/useAuth"
 import { useState } from "react";
 import React from "react";
 import axios from "axios";
+// import { format } from 'date-fns'
 
 const AddPatternForm = (props) => {
 
 
-    const [patterns, setPatterns] =useState([]);
     const [pdf, setPdf] = useState("");
     const [patternName, setPatternName] = useState("");
     const [artist, setArtist] = useState("");
     const [date, setDate] = useState("");
     const [embroidery, setEmbroidery] = useState(false);
     const [crossStitch, setCrossStitch] = useState(false);
-    const [photo, setPhoto] = useState(null);
     const [user, token] = useAuth()
 
 
-    function handleAdd(event) {
-        event.preventDefault();
-        let newEntry = {
-            pattern_pdf: pdf,
-            pattern_name: patternName,
-            artist: artist,
-            date_added: date,
-            is_embroidery: embroidery,
-            is_cross_stitch: crossStitch,
-            user: user.id,
-        };
-        debugger
-        console.log('new entry', newEntry);
-        addUserPattern(newEntry);
-    };
+
+        function handleDateChange(event) {
+          const selectedDate = new Date(event.target.value);
+          const formattedDate = selectedDate.toISOString().slice(0,10);
+          setDate(formattedDate);
+        }
 
 
-    const addUserPattern = async (newEntry) => {
+    const addUserPattern = async () => {
+      const formData = new FormData();
+          formData.append("pattern_pdf", pdf);
+          formData.append("pattern_name", patternName);
+          formData.append("artist", artist);
+          formData.append("date_added", date);
+          formData.append("is_embroidery", embroidery);
+          formData.append("is_cross_stitch", crossStitch);
         try {
-          let response = await axios.post('http://127.0.0.1:8000/api/patterns/user/', newEntry, {
+          let response = await axios.post('http://127.0.0.1:8000/api/patterns/user/', formData, {
             headers: {
               Authorization: "Bearer " + token,
+              "Content-Type": "multipart/form-data",
             },
           });
-          console.log("pattern data from AddPattern Form", response.data)
-          setPatterns(response.data);
+          console.log("set response", response.data)
+          setPdf("");
+          setPatternName("");
+          setArtist("");
+          setDate("");
+          setEmbroidery(false);
+          setCrossStitch(false);
         } catch (error) {
-          console.log(error.response)
         }
+      };
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        addUserPattern();
       };
 
 
   return (
-    <form onSubmit={(event) => handleAdd(event)}>
+    <form onSubmit={handleSubmit}>
       <h2>Add A New Pattern</h2>
       <ol>
         <li>
           <label>PDF File</label>
-          <input type="file" name="pdf" onChange={(event) => setPdf(event.target.value)}/>
+          <input type="file" name="pdf" onChange={(event) => setPdf(event.target.files[0])}/>
         </li>
 
         <li>
@@ -66,14 +73,14 @@ const AddPatternForm = (props) => {
         <input type="text" name="artist" onChange={(event) => setArtist(event.target.value)} />
         <li>
           <label>Date</label>
-          <input type="date" name="date" onChange={(event) => setDate(event.target.value)} />
+          <input type="date" name="date" onChange={handleDateChange} />
         </li>
 
         <li>
-          <label>Emboridery?</label>
-          <input type="radio"  name="embroidery" value="true" onChange={(event) => setEmbroidery(event.target.value)}/>
+          <label>Embroidery?</label>
+          <input type="checkbox"    name="false" onChange={(event) => setEmbroidery(event.target.value)}/>
           <label> or Cross Stitch?</label>
-          <input type="radio" name="crossStitch" value="true" onChange={(event) => setCrossStitch(event.target.value)}/>
+          <input type="checkbox"  name="false" onChange={(event) => setCrossStitch(event.target.value)}/>
         </li>
 
         <button type="submit">Submit Pattern</button>
