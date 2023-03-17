2d0b889b10045193ffe4ab552c2e0e678509c444
Mara Green <maragreen42@gmail.com>
2023-03-16 20:42:34 -0700

    Changed the graph on the homePage to a bar graph

--- frontend/src/components/DisplayGraph.jsx
+++ frontend/src/components/DisplayGraph.jsx
@@ -6,14 +6,29 @@ import { useEffect } from "react";
 import axios from "axios";
 
 import "../../src/pages/HomePage/HomePage.css"
-
-
-
+import { Chart } from "react-google-charts";
 
 const DisplayGraph = (props) => {
 
   const [user,token] = useAuth();
   const [photos, setPhotos] = useState([])
+
+  const getMonthName = (monthNumber) => {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return months[monthNumber - 1];
+  };
+  
+  const getCompletedProjectsByMonth = (photos) => {
+    const completedProjectsByMonth = new Array(12).fill(0);
+  
+    photos.forEach((photo) => {
+      const dateFinished = new Date(photo.date_finished);
+      const month = dateFinished.getMonth();
+      completedProjectsByMonth[month]++;
+    });
+  
+    return completedProjectsByMonth;
+  };
 
 
   useEffect(() => {
@@ -35,42 +50,109 @@ const DisplayGraph = (props) => {
     fetchUserPhotos();
   }, [token]);
 
-  const dateFinished = photos.map((photo) => {
-    return photo.date_finished;
-  })
-  // console.log('dateFinished',dateFinished);
+  // const dateFinished = photos.map((photo) => {
+  //   return photo.date_finished;
+  // })
 
-  return ( 
-    <div className="calendar-heatmap">
-      <div className="graph-h2">
-      <h2 >Projects completed this year:</h2>
-      </div>
-      <CalendarHeatmap
-  startDate={new Date('2023-01-01')}
-  endDate={new Date('2023-12-31')}
-  values= {dateFinished.map((date) => ({
-    date: date,
-    count: 1,
-  }))}
+  const completedProjectsByMonth = getCompletedProjectsByMonth(photos);
+
+  const data = [["Month", "Completed Projects", { role: "style" }]];
+
+  completedProjectsByMonth.forEach((completedProjects, monthIndex) => {
+    const monthName = getMonthName(monthIndex + 1);
+    data.push([monthName, completedProjects, "color: #b87333"]);
+  });
 
   
-  classForValue={(value) => {
-    if (!value) {
-      return 'color-empty';
-    }
-    return `color-scale-${value.count}`;
-  }}
-  showWeekdayLabels = {true}
-  showMonthLabels = {true}
-   showOutOfRangeDays = {true}
-   gutterSize = {1}
-  onClick={value => alert(`Clicked on value with count: ${value.count}`)}
-/>      
-
+  return ( 
+    <>
+    <div className="bar-chart">
+    <Chart 
+        chartType="ColumnChart"
+        width="150%"
+        height="400px"
+        data={data}
+        legendToggle
+      />
+    {/* <Chart
+      chartType="ColumnChart"
+      width="100%"
+      height="400px"
+      data={[["Month", "Completed Projects"], [4, 5.5], [8, 12]]}
+      legendToggle
+    /> */}
     </div>
+    </>
    );
-  }
-
-
-
+}
+ 
 export default DisplayGraph;
+
+
+
+
+
+// const DisplayGraph = (props) => {
+
+//   const [user,token] = useAuth();
+//   const [photos, setPhotos] = useState([])
+
+
+//   useEffect(() => {
+//     console.log('display graph ran')
+//     const fetchUserPhotos=async () => {
+//       try {
+//         let response = await axios.get('http://127.0.0.1:8000/api/patterns/photos/1/', {
+//           headers: {
+//             Authorization: "Bearer " +token,
+
+//           },
+//         });
+//         console.log('photo array:', response.data)
+//         setPhotos(response.data);
+//       } catch (error) {
+//         console.log(error.response)
+//       }
+//     };
+//     fetchUserPhotos();
+//   }, [token]);
+
+//   const dateFinished = photos.map((photo) => {
+//     return photo.date_finished;
+//   })
+//   // console.log('dateFinished',dateFinished);
+
+//   return ( 
+//     <div className="calendar-heatmap">
+//       <div className="graph-h2">
+//       <h2 >Projects completed this year:</h2>
+//       </div>
+//       <CalendarHeatmap
+//   startDate={new Date('2023-01-01')}
+//   endDate={new Date('2023-12-31')}
+//   values= {dateFinished.map((date) => ({
+//     date: date,
+//     count: 1,
+//   }))}
+
+  
+//   classForValue={(value) => {
+//     if (!value) {
+//       return 'color-empty';
+//     }
+//     return `color-scale-${value.count}`;
+//   }}
+//   showWeekdayLabels = {true}
+//   showMonthLabels = {true}
+//    showOutOfRangeDays = {true}
+//    gutterSize = {1}
+//   onClick={value => alert(`Clicked on value with count: ${value.count}`)}
+// />      
+
+//     </div>
+//    );
+//   }
+
+
+
+// export default DisplayGraph;
